e65c5027b6c8c1a4a4b0420c4193db93d2ce8415
shravanrn <shravanrn@gmail.com>
2024-07-22 19:32:27 -0500

    some win scripts for segment clobber

--- build_segment_clobber_test.bat
+++ build_segment_clobber_test.bat
@@ -1,2 +1,3 @@
+del segments_clobber_test.exe
 "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 cl.exe segments_clobber_test.c /link /out:segments_clobber_test.exe
